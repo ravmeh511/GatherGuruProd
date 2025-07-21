45782fe7eb339e396aab26400afaf94f492cca94
Ravi Mehta <mehtaravi83@gmail.com>
2025-07-21 18:05:12 -0400

    updatee

--- Backend/server.js
+++ Backend/server.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 // CORS configuration
 const allowedOrigins = process.env.ALLOWED_ORIGINS 
     ? process.env.ALLOWED_ORIGINS.split(',') 
-    : ['http://localhost:5173', 'http://localhost:5174'];
+    : ['http://localhost:5173'];
 
 app.use(cors({
     origin: function (origin, callback) {
